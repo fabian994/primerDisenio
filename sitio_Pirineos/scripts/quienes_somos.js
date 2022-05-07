8caec0d7bb741cb1cb341372eb4c8a1c6dc83d9e
Jazzareth <A01367882@tec.mx>
2022-05-07 13:44:35 -0500

    eliminar función

--- sitio_Pirineos/scripts/quienes_somos.js
+++ sitio_Pirineos/scripts/quienes_somos.js
@@ -229,59 +229,6 @@ function last_ev(){
 }
 
 
-$(window).load(function () {
-    $(".trigger_popup_fricc").click(function(){
-       $('.hover_bkgr_fricc').show();
-    });
-    $('.popupCloseButton').click(function(){
-        $('.hover_bkgr_fricc').hide();
-    });
-});
-
-$(window).load(function () {
-    $(".trigger_popup_fricc_1").click(function(){
-       $('.hover_bkgr_fricc_1').show();
-    });
-    $('.popupCloseButton_1').click(function(){
-        $('.hover_bkgr_fricc_1').hide();
-    });
-});
-
-$(window).load(function () {
-    $(".trigger_popup_fricc_2").click(function(){
-       $('.hover_bkgr_fricc_2').show();
-    });
-    $('.popupCloseButton_2').click(function(){
-        $('.hover_bkgr_fricc_2').hide();
-    });
-});
-
-$(window).load(function () {
-    $(".trigger_popup_fricc_3").click(function(){
-       $('.hover_bkgr_fricc_3').show();
-    });
-    $('.popupCloseButton_3').click(function(){
-        $('.hover_bkgr_fricc_3').hide();
-    });
-});
-
-$(window).load(function () {
-    $(".trigger_popup_fricc_4").click(function(){
-       $('.hover_bkgr_fricc_4').show();
-    });
-    $('.popupCloseButton_4').click(function(){
-        $('.hover_bkgr_fricc_4').hide();
-    });
-});
-
-$(window).load(function () {
-    $(".trigger_popup_fricc_5").click(function(){
-       $('.hover_bkgr_fricc_5').show();
-    });
-    $('.popupCloseButton_5').click(function(){
-        $('.hover_bkgr_fricc_5').hide();
-    });
-});
 
 
 
